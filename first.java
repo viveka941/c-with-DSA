6b9bf352ac9d9d114c98bf74ff895ceeb79d7a4a
Vivek Kumar <viveka9415@gmail.com>
2024-06-11 12:03:17 +0530

    factorial function add

--- first.java
+++ first.java
@@ -1,6 +1,21 @@
-class first{
+class first {
+  public static void intor(String str, int age) {
+    System.out.println("Name is " + str + " age " + age);
+  }
+
+  public static void factorial(int num) {
+    int res = 1;
+    for (int i = 1; i < num; i++) {
+      res = res * i;
+
+      System.out.println(res);
+    }
+    // System.out.println(res);
+  }
+
   public static void main(String[] args) {
-    System.out.println("hii");
-    System.out.println("hii everyone");
+
+    factorial(5);
+
   }
 }
