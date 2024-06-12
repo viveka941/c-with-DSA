cbe67cb87befdb73afbf614dd3ca3621f21f22c5
Vivek Kumar <viveka9415@gmail.com>
2024-06-12 09:44:25 +0530

    add nCr function

--- first.java
+++ first.java
@@ -3,19 +3,26 @@ class first {
     System.out.println("Name is " + str + " age " + age);
   }
 
-  public static void factorial(int num) {
+  public static int factorial(int num) {
     int res = 1;
     for (int i = 1; i < num; i++) {
       res = res * i;
 
       
     }
-    System.out.println(res);
+  return res;
+  }
+
+  public static int nCr(int n ,int r) {
+    int num = factorial(n);
+    int dum = factorial(r) * factorial(n-r);
+    return num/dum;
   }
 
   public static void main(String[] args) {
 
-    factorial(5);
+    // System.out.println(factorial(4));
+    System.out.println(nCr(5, 2));
 
   }
 }
