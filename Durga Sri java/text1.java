15ee5fda45ff28f5610ac7f432e3732f7a956e1a
Vivek Kumar <viveka9415@gmail.com>
2024-06-14 11:56:50 +0530

    add some point

---
+++ Durga Sri java/text1.java
@@ -0,0 +1,19 @@
+
+
+ class text2 {
+  public static void main(String[] args) {
+System.out.println("A");
+  }
+}
+
+ class text {
+  public static void main(String[] args) {
+System.out.println("B");
+  }
+}
+
+public class text1 {
+  public static void main(String[] args) {
+System.out.println("Main");
+  }
+}
