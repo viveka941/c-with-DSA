741d67c279ff4ccdf69a4807b9f8ef642fad4596
Vivek Kumar <viveka9415@gmail.com>
2024-06-18 10:25:35 +0530

    oops with durga sir

---
+++ Durga Sri java/interf.java
@@ -0,0 +1,19 @@
+
+interface interf {
+
+  public void m1();
+
+  public void m2();
+}
+
+abstract class serviePorvider implements interf {
+  public void m1() {
+
+  }
+}
+
+class subservice extends serviePorvider {
+public void m2(){
+  
+}
+}
